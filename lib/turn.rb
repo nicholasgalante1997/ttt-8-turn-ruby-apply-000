1298bcfd6549c68f13f8a4067ce3a08c9325c5e0
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-16 17:46:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,7 +48,7 @@ def turn(board)
   puts "Please enter 1-9:"
   num = gets.strip
   index = input_to_index(num)
-  if valid_move(board, index) == true
+  if valid_move?(board, index) == true
     move(board, index)
     display_board(board)
   else
