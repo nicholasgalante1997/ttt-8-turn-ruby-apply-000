5aac524c7ff8a63060cdfb9d53d7d7da13169824
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-16 17:12:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,28 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  def position_taken? (array, ind)
+    if array[ind] == "" || array[ind] == " " || array[ind] == nil
+    return false
+  else
+    return true
+  end
+  end
+  
+  def on_board?(number)
+    if number.between?(0, 8)
+      return true 
+    else
+      return false 
+    end
+  end
+  
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+end
+
